51581161487bc92b2ee7c697f1f74ed55a29163b
Mohanapitaka <mohanapitaka@gmail.com>
2025-05-23 19:26:31 +0530

    Create Relationaloperators.java

---
+++ Relationaloperators.java
@@ -0,0 +1,10 @@
+class Relationaloperators{
+  public static void main(String[] args){
+    int x=4;
+    int y=5;
+    boolean res= x>y;
+    boolean result= x!=y;
+    System.out.println(res);
+    System.out.println(result);
+  }
+}
