736e40767c7a8b340b8e4a0d46f916ebe516f7b6
Mohanapitaka <mohanapitaka@gmail.com>
2025-05-23 18:54:56 +0530

    Create Datatypes.java

---
+++ Datatypes.java
@@ -0,0 +1,13 @@
+class Datatypes{
+public static void main(String[] args){
+  int num1=9;
+  byte by=127;
+  short sh=558;
+  long l=5854l;
+  float f=2.3f;
+  double d=9.1;
+  char c='c';
+  boolean b =true;
+}
+}
+  
