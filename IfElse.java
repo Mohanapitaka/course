2ecfffdb0f8a81a0022961e670d4e70fed44a4e3
Mohanapitaka <mohanapitaka@gmail.com>
2025-06-14 12:18:03 +0530

    Create IfElse.java

---
+++ IfElse.java
@@ -0,0 +1,12 @@
+class IfElse{
+  public static void main(String[]args){
+    int a=10;
+    int b=5;
+    if(a>b){
+      System.out.println("true");
+    }
+    else{
+      System.out.println("false");
+    }
+  }
+}
