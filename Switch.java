ad1fea1f45342a9bda39cc4d83d7f5dda094fc28
Mohanapitaka <mohanapitaka@gmail.com>
2025-06-14 12:36:25 +0530

    Create Switch.java

---
+++ Switch.java
@@ -0,0 +1,12 @@
+class Switch{
+  public static void main(String[] args){
+    String day="wed";
+    String resut="";
+    result=Switch(day){
+      case"Saturday","Sunday"->"6am";
+      case"Monday"->"8am";
+      default->"7am";
+    }
+    System.out.println(result);
+  }
+}
